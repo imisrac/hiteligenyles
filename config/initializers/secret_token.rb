f09cdd15cd6c41080fc1523619d6596e31265ed9
Németh Imre <imisrac@gmail.com>
2012-05-12 17:07:46 +0200

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Hiteligenyles::Application.config.secret_token = '10557a934ad38f037ae053ff58beb67b14919fc6048210fb60623f78f2cb63a988146e2d7d746a55f135082f2b25f24335fc50125ceb4eaebb3cd5ad14d0f442'
