ba4a1fa056355a1c2eaf0d531f38717d78b495dc
Németh Imre <imisrac@gmail.com>
2012-05-13 01:42:36 +0200

    Finish user signup.

--- app/controllers/claims_controller.rb
+++ app/controllers/claims_controller.rb
@@ -1,4 +1,19 @@
 class ClaimsController < ApplicationController
   def new
+    @claim = Claim.new
+  end
+
+  def create
+    @claim = Claim.new(params[:claim])
+    if @claim.save
+      flash[:success] = "Kérelem benyújtva"
+      redirect_to @claim
+    else
+      render 'new'
+    end
+  end
+
+  def show
+    @claim = Claim.find(params[:id])
   end
 end
